7142c68ca6f1f4d18fc341cf6f59de95237155ea
volbil <volbilnexus@gmail.com>
2021-09-10 23:37:20 +0900

    Updated port

--- dns.py
+++ dns.py
@@ -21,5 +21,5 @@ def wildcard_example(query):
 
 if __name__ == "__main__":
     ns.settings.SERVER_ADDRESS = "0.0.0.0"
-    ns.settings.SERVER_PORT = 9001
+    ns.settings.SERVER_PORT = 53
     ns.run()
