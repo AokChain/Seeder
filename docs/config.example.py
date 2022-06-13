d1d28aa763455f2f64b625ef64418d0f7fbe1f5e
volbil <volbilnexus@gmail.com>
2022-06-13 19:48:27 +0300

    Added config example

---
+++ docs/config.example.py
@@ -0,0 +1,20 @@
+secret = "Lorem ipsum dor sit amet"
+host = "0.0.0.0"
+debug = False
+port = 7979
+
+magic = "3BEEE002"
+
+db = {
+    "provider": "sqlite",
+    "filename": "../seed.db",
+    "create_db": True
+}
+
+interval = {
+    "minutes": 5
+}
+
+init = [
+    ["211.216.3.214", 33441]
+]
